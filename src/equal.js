4f41e3221e3a32a36dcfab64607d6ca28f23fe40
Henry Pap <henry.pap@modis.com>
2020-10-09 17:41:23 +0200

    equal checks on props as well

--- src/equal.js
+++ src/equal.js
@@ -1,28 +1,29 @@
 export default function (prevProps, nextProps) {
   if (prevProps.error !== nextProps.error) return false;
   if (prevProps.isTouched !== nextProps.isTouched) return false;
-
+  if (!checkObject(prevProps.props, nextProps.props)) return false;
+  
   if (prevProps.value instanceof Object) {
-    if (
-      Object.keys(prevProps.value).length !==
-      Object.keys(nextProps.value).length
-    )
-      return false;
-    for (const key in prevProps.value) {
-      if (!nextProps.value[key]) return false;
-      if (prevProps.value[key] !== nextProps.value[key]) return false;
-    }
-
-    return true;
+    return checkObject(prevProps.value, nextProps.value);
   }
   if (prevProps.value instanceof Array) {
     if (prevProps.value.length !== nextProps.value.length) return false;
     for (let i = 0; i < prevProps.value.length; i++) {
       if (prevProps.value[i] !== nextProps.value[i]) return false;
     }
-
+    
     return true;
   }
+    
+  if (prevProps.value !== nextProps.value) return false;
+}
 
-  return prevProps.value === nextProps.value;
+function checkObject(a, b) {
+  if (Object.keys(pa).length !== Object.keys(b).length) return false;
+  for (const key in pa) {
+    if (!b[key]) return false;
+    if (pa[key] !== b[key]) return false;
+  }
+  
+  return true;
 }
